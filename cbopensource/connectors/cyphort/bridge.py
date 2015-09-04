232ddf743db82f6ef4f2bd8ad08bb679d1498421
Jason Garman <jgarman@bit9.com>
2015-09-04 12:34:26 -0400

    Update default binary filters

--- cbopensource/connectors/cyphort/bridge.py
+++ cbopensource/connectors/cyphort/bridge.py
@@ -123,7 +123,7 @@ class CyphortConnector(DetonationDaemon):
     @property
     def filter_spec(self):
         filters = []
-        max_module_len = 10 * 1024 * 1024
+        max_module_len = 15 * 1024 * 1024
         filters.append('(os_type:windows OR os_type:osx) orig_mod_len:[1 TO %d]' % max_module_len)
         additional_filter_requirements = self.get_config_string("binary_filter_query", None)
         if additional_filter_requirements:
